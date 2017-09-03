8ca0273b33b9743ebc5970413a6bd0a2bee9b487
Marja Hölttä <marja.holtta@gmail.com>
2017-09-03 14:43:21 +0200

    GC: Getting started. Pointer discovery.

--- runtime/memory.c
+++ runtime/memory.c
@@ -3,8 +3,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CHUNK_SIZE 4096
+
+// FIXME: this simple version has only one memory chunk. Use a list instead.
+void* current_chunk = 0;
+void* current_chunk_cursor = 0;
+void* current_chunk_end = 0;
+
+void* other_chunk = 0;
+void* other_chunk_cursor = 0;
+void* other_chunk_end = 0;
+
+void* stack_high = 0;
+
+int is_in_current_chunk(void* p) {
+  return p > current_chunk && p < current_chunk_end;
+}
+
+void memory_init(void* stack) {
+  fprintf(stderr, "Runtime starting. Stack is at %p.\n", stack);
+  stack_high = stack;
+}
+
+void memory_teardown() {
+  free(current_chunk);
+  free(other_chunk);
+}
+
+void do_gc();
+
 void* runtime_allocate(int size) {
-  // FIXME: gc
-  void* v = malloc(size);
-  return v;
+  fprintf(stderr, "Allocate %d\n", size);
+  if (current_chunk == 0) {
+    // First allocation.
+    current_chunk = malloc(CHUNK_SIZE);
+    current_chunk_cursor = current_chunk;
+    current_chunk_end = current_chunk + CHUNK_SIZE;
+    fprintf(stderr, "First chunk: %p %p\n", current_chunk_cursor, current_chunk_end);
+
+    void* other_chunk = malloc(CHUNK_SIZE);
+  }
+
+  void* result = 0;
+  if (current_chunk_end - current_chunk_cursor > size) {
+    fprintf(stderr, "Fits in the current chunk\n");
+    result = current_chunk_cursor + 1;
+    void** p = (void**)current_chunk_cursor;
+    *p = (void*)size;
+    current_chunk_cursor += (size + 1);
+  } else {
+    // Collect garbage.
+    do_gc();
+  }
+
+  return result;
+}
+
+void do_gc() {
+  // Discover potential pointers. They can be in the stack or in the current
+  // memory chunk, pointed to by already discovered pointers.
+  int stack_is_here = 0;
+  void* stack_low = &stack_is_here;
+  void* p;
+
+  fprintf(stderr, "stack %p %p\n", stack_low, stack_high);
+  for (p = stack_low; p < stack_high; ++p) {
+    int* p2 = (int*)(p);
+    void* p3 = (void*)(*p2);
+    if (is_in_current_chunk(p3)) {
+      fprintf(stderr, "Found pointer %p\n", p3);
+    }
+  }
+
 }
--- runtime/runtime.c
+++ runtime/runtime.c
@@ -1,18 +1,18 @@
+#include "memory.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
 extern void user_code(void);
 
 void* runtime_GetGlobalsTable(int globals_size) {
-  // FIXME: gc
-  void* v = malloc(4 * globals_size);
+  void* v = runtime_allocate(4 * globals_size);
   fprintf(stderr, "GetGlobalsTable %d returns %p\n", globals_size, v);
   return v;
 }
 
 void* runtime_CreateFunctionContext(void* previous, void* outer, int params_size) {
-  // FIXME: gc
-  void** context = (void**)malloc(4 * (3 + params_size));
+  void** context = (void**)runtime_allocate(4 * (3 + params_size));
   context[0] = previous;
   context[1] = outer;
   context[2] = 0;
@@ -21,8 +21,10 @@ void* runtime_CreateFunctionContext(void* previous, void* outer, int params_size
 }
 
 int main(int argc, char** argv) {
-  fprintf(stderr, "Runtime starting\n");
+  int stack_is_here = 0;
+  memory_init(&stack_is_here);
   user_code();
+  memory_teardown();
   fprintf(stderr, "Runtime exiting\n");
   return 0;
 }
